71a49f9481fd1281a2766c6cb58f99e059e5d336
Alexandr Noskov <san9nosko8@gmail.com>
2023-12-10 11:17:56 +0300

    Domashka 24.1

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+NULLABLE = {
+    'null': True,
+    'blank': True
+}
+
+
+class UserRoles(models.TextChoices):
+    """ Класс на создание ролей рользователя """
+
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
+
+
+# Create your models here.
+class User(AbstractUser):
+    """Модель пользователя(Юзера)"""
+    username = None
+    email = models.EmailField(max_length=30, unique=True, verbose_name='Почта')
+    phone = models.CharField(max_length=20, verbose_name='Телефон', **NULLABLE)
+    city = models.CharField(max_length=20, verbose_name='Город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users', verbose_name='Аватар', **NULLABLE)
+    # Роль юзера - модератор или просто польлзователь
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
