11329560edec1a95be292a59be1e944ffb61409e
Alexandr Noskov <san9nosko8@gmail.com>
2024-01-28 18:43:49 +0300

    docker, docker-compose.yaml

--- config/settings.py
+++ config/settings.py
@@ -180,7 +180,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
